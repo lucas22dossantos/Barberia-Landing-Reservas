7543f1bea1304a53f77f93a2ab76eb311560014e
Lucas Emanuel Dos Santos <lucas.emanuel.dossantos40@gmail.com>
2025-12-02 13:04:34 -0300

    pantalla de Olvidé mi contraseña

--- src/admin/AdminRouter.jsx
+++ src/admin/AdminRouter.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Reservas from "./pages/Reservas";
 import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
 import AdminLayout from "./layout/AdminLayout";
 import Citas from "./pages/Citas";
 import Sericios from "./pages/Servicios";
@@ -10,13 +11,17 @@ import Ajustes from "./pages/Ajustes";
 export default function AdminRouter() {
   return (
     <Routes>
+      {/* login */}
+      <Route path="/admin/login" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+
+      {/* panel de admin */}
       <Route path="/admin" element={<AdminLayout />}>
         <Route path="/admin" element={<Dashboard />} />
         <Route path="citas" element={<Citas />} />
         <Route path="Servicios" element={<Sericios />} />
         <Route path="Ajustes" element={<Ajustes />} />
         <Route path="/admin/reservas" element={<Reservas />} />
-        <Route path="/admin/login" element={<Login />} />
       </Route>
     </Routes>
   );
