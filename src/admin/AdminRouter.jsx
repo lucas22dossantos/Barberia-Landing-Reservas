2ed7e55c9d810b1c5c3a938130ed08432a82490c
Lucas Emanuel Dos Santos <lucas.emanuel.dossantos40@gmail.com>
2025-12-02 13:36:11 -0300

    se agrego la  pantalla ResetPassword con validación simulada y mensaje de token expirado

--- src/admin/AdminRouter.jsx
+++ src/admin/AdminRouter.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import AdminLayout from "./layout/AdminLayout";
 import Dashboard from "./pages/Dashboard";
 import Reservas from "./pages/Reservas";
-import Login from "./pages/Login";
-import ForgotPassword from "./pages/ForgotPassword";
-import AdminLayout from "./layout/AdminLayout";
 import Citas from "./pages/Citas";
 import Sericios from "./pages/Servicios";
 import Ajustes from "./pages/Ajustes";
+
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 
 export default function AdminRouter() {
   return (
@@ -14,6 +16,7 @@ export default function AdminRouter() {
       {/* login */}
       <Route path="/admin/login" element={<Login />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
 
       {/* panel de admin */}
       <Route path="/admin" element={<AdminLayout />}>
